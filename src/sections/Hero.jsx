51e8582450f1953de000009fe1dfd7646afae7fb
ljsvdn <lajosvadnai@gmail.com>
2024-03-05 20:04:29 +0100

    create PopularProducts section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -54,9 +54,9 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-          {shoes.map((shoe) => {
+          {shoes.map((shoe, index) => {
             return (
-              <div key={shoe}>
+              <div key={index}>
                 <ShoeCard
                   imgURL={shoe}
                   changeBigShoeImage={(shoe) => {
